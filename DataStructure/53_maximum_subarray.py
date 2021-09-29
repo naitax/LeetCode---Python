e9cd6b34bc8a45b3164460974374f1e58ea50e25
naitax <73228371+naitax@users.noreply.github.com>
2021-09-29 22:06:18 +0100

    Create 53_maximum_subarray.py
    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
    
    A subarray is a contiguous part of an array.
    
     Example 1:
    
    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
    Output: 6
    Explanation: [4,-1,2,1] has the largest sum = 6.
    
    Example 2:
    
    Input: nums = [1]
    Output: 1
    
    Example 3:
    
    Input: nums = [5,4,-1,7,8]
    Output: 23

---
+++ DataStructure/53_maximum_subarray.py
@@ -0,0 +1,53 @@
+def maxSubArraySum(a, size):
+    max_so_far = a[0]
+    max_ending_here = 0
+
+    for i in range(0, size):
+        max_ending_here = max_ending_here + a[i]
+        if max_ending_here < 0:
+            max_ending_here = 0
+
+        # Do not compare for all elements. Compare only
+        # when  max_ending_here > 0
+        elif (max_so_far < max_ending_here):
+            max_so_far = max_ending_here
+
+    return max_so_far
+#
+
+# Driver function to check the above function
+a = [-2, -3, 4, -1, -2, 1, 5, -3]
+print("Maximum contiguous sum is", maxSubArraySum(a, len(a)))
+
+######## first test
+def max_sub_array(a):
+    max_sum = a[0]
+    current_value = 0
+
+    for i in range(0, len(a)):
+        current_value = current_value + a[i]
+        if current_value < 0:
+            current_value = 0
+        elif max_sum<current_value:
+            max_sum = current_value
+
+    return max_sum
+
+
+print(max_sub_array(a))
+
+#### second test
+def test(a, size):
+
+    max_value = a[0]
+    current = 0
+    for i in range(0, size):
+        current = current + a[i]
+        if current < 0:
+            current = 0
+        elif max_value < current:
+            max_value = current
+
+    return max_value
+
+print(test(a, len(a)))
