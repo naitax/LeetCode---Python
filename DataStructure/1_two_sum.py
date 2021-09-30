525056b1f688f010e69e0ae6f1e61eb06758d3ba
naitax <73228371+naitax@users.noreply.github.com>
2021-09-30 21:01:09 +0100

    Create 1_two_sum.py
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
     
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
    
    Example 2:
    
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    
    Example 3:
    
    Input: nums = [3,3], target = 6
    Output: [0,1]

---
+++ DataStructure/1_two_sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashTable = dict()
+        pair = []
+        i = 0
+        foundPair = False
+
+        while i < len(nums):
+            hashTable[nums[i]] = i
+            i += 1
+
+
+        i = 0
+
+        while i < len(nums) and not foundPair:
+            compliment = target - nums[i]
+
+            if compliment in hashTable:
+                if i != hashTable[compliment]:
+                    pair.append((i))
+                    pair.append(hashTable[compliment])
+                    foundPair = True
+            i += 0
+        return pair
+      
+ #https://medium.com/swlh/understanding-leetcode-the-two-sum-problem-6428ed5cb18d
